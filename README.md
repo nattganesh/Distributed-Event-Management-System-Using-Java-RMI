2986e60fd0cce80515e5ce6f278f877192478da9
gursimransingh <gursimran81@gmail.com>
2019-06-06 15:11:41 -0400

    Added few threads in  MultiClient.java

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Comp6231-A1
+Assignment 1
+Distributed Event Management System
+
+Names                        ID
+
+Gursimran Singh -            40080981
+Natheepan Ganeshamoorthy -   29335838
+
--- RMI/src/Client/MultiClient.java
+++ RMI/src/Client/MultiClient.java
@@ -14,13 +14,15 @@ import java.rmi.registry.Registry;
 public class MultiClient {
     public static void main(String[] args) throws RemoteException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry(CommonUtils.OTTAWA_SERVER_PORT);
+        Registry registrymon = LocateRegistry.getRegistry(CommonUtils.MONTREAL_SERVER_PORT);
 
-        ServerInterface serverInterface = (ServerInterface) registry.lookup(CommonUtils.OTTAWA_SERVER_NAME);
+        ServerInterface serverInterfaceOttawa = (ServerInterface) registry.lookup(CommonUtils.OTTAWA_SERVER_NAME);
+        ServerInterface serverInterfaceMontreal = (ServerInterface) registrymon.lookup(CommonUtils.MONTREAL_SERVER_NAME);
 
         Runnable runnable1 = () ->
         {
             try {
-                String response = serverInterface.bookEvent("OTWC3425", "OTWE030619", CommonUtils.CONFERENCE, "15");
+                String response = serverInterfaceOttawa.bookEvent("OTWC3425", "MTLM130720", CommonUtils.CONFERENCE, "25");
                 System.out.println("Thread 1: " + Thread.currentThread().getName() + "Respnse from server: " + response);
             } catch (RemoteException e) {
                 e.printStackTrace();
@@ -32,7 +34,7 @@ public class MultiClient {
         Runnable runnable2 = () ->
         {
             try {
-                String response = serverInterface.bookEvent("OTWC3425", "OTWE030619", CommonUtils.CONFERENCE, "15");
+                String response = serverInterfaceOttawa.bookEvent("OTWC3425", "TORE101022", CommonUtils.SEMINAR, "15");
                 System.out.println("Thread 2: " + Thread.currentThread().getName() + "Respnse from server: " + response);
             } catch (RemoteException e) {
                 e.printStackTrace();
@@ -44,7 +46,7 @@ public class MultiClient {
         Runnable runnable3 = () ->
         {
             try {
-                String response = serverInterface.bookEvent("OTWC3425", "OTWE030619", CommonUtils.CONFERENCE, "15");
+                String response = serverInterfaceOttawa.bookEvent("OTWC3425", "OTWA090619", CommonUtils.TRADESHOW, "80");
                 System.out.println("Thread 3: " + Thread.currentThread().getName() + "Respnse from server: " + response);
             } catch (RemoteException e) {
                 e.printStackTrace();
@@ -54,9 +56,37 @@ public class MultiClient {
         Thread thread3 = new Thread(runnable3);
         thread3.setName("Thread 3");
 
+        Runnable runnable4 = () ->
+        {
+            try {
+                String response = serverInterfaceOttawa.bookEvent("OTWC3456", "OTWA090619", CommonUtils.TRADESHOW, "33");
+                System.out.println("Thread 4: " + Thread.currentThread().getName() + "Respnse from server: " + response);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread thread4 = new Thread(runnable4);
+        thread4.setName("Thread 4");
+
+        Runnable runnable5 = () ->
+        {
+            try {
+                String response = serverInterfaceMontreal.addEvent("MTLM130720", CommonUtils.CONFERENCE, "33", "MTLM130720");
+                System.out.println("Thread 5: " + Thread.currentThread().getName() + "Respnse from server: " + response);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread thread5 = new Thread(runnable5);
+        thread4.setName("Thread 5");
+
         thread1.start();
         thread2.start();
         thread3.start();
+        thread4.start();
+        thread5.start();
 
     }
 }
